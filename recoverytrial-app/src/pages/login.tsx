4a3ccb6f36f52d8bdcd41fd71f93b6820244ae3d
marialuisacampos <luisa.campos@ufrpe.br>
2022-08-02 13:45:38 -0300

    Add Login and Signup pages

---
+++ recoverytrial-app/src/pages/login.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+
+type Props = {};
+
+const login = (props: Props) => {
+  return (
+    <div>
+      <Header />
+      <Footer />
+    </div>
+  );
+};
+
+export default login;
