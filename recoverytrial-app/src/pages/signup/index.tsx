158714bdd18cb60cc69253ff31869e2723f4e173
marialuisacampos <luisa.campos@ufrpe.br>
2022-08-15 16:12:27 -0300

    Create Register Page

--- recoverytrial-app/src/pages/signup/index.tsx
+++ recoverytrial-app/src/pages/signup/index.tsx
@@ -1,9 +1,402 @@
-import React from "react";
+import React, { useState } from "react";
+import Button from "../../components/Button";
+import Error from "../../components/Error";
+import Footer from "../../components/Footer";
+import Navbar from "../../components/Navbar";
 
 interface Props {}
 
-const signup = (props: Props) => {
-  return <div>signup</div>;
+const Signup = (props: Props) => {
+  const [nome, setNome] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
+  const [senha, setSenha] = useState<string>("");
+  const [telefone, setTelefone] = useState<string>("");
+  const [idade, setIdade] = useState<number>();
+  const [sexo, setSexo] = useState<string>("");
+  const [praticaAtividade, setPraticaAtividade] = useState<boolean>();
+  const [fraquezaMuscular, setFraquezaMuscular] = useState<boolean>();
+  const [dificuldadeRespiratoria, setDificuldadeRespiratoria] =
+    useState<boolean>();
+  const [perdaDeMemoria, setPerdaDeMemoria] = useState<boolean>();
+  const [acidenteEm12Meses, setAcidenteEm12Meses] = useState<boolean>();
+  const [hipertensao, setHipertensao] = useState<boolean>();
+  const [diabetes, setDiabetes] = useState<boolean>();
+  const [doencasCardio, setDoencasCardio] = useState<boolean>();
+  const [doencasPulmonares, setDoencasPulmonares] = useState<boolean>();
+
+  const [error, setError] = useState<string>("");
+
+  const handleRegister = (e: any) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div>
+      <Navbar />
+      <main>
+        <div className="bg-gray-300 flex flex-col max-w-[300px] m-auto pt-4 px-8 rounded-xl justify-center items-center pb-4 md:pb-2 md:max-w-2xl">
+          <form className="flex flex-col max-w-xs gap-2 pt-4 md:flex-row md:max-w-lg md:flex-wrap justify-center items-center">
+            <div className="flex flex-col pb-4">
+              <label className="label" htmlFor="nome">
+                Nome
+              </label>
+              <input
+                className="input"
+                type="text"
+                value={nome}
+                name="nome"
+                onChange={(e) => [setNome(e.target.value), setError("")]}
+              />
+            </div>
+
+            <div className="flex flex-col pb-4">
+              <label className="label" htmlFor="email">
+                Email
+              </label>
+              <input
+                className="input"
+                type="text"
+                value={email}
+                name="email"
+                onChange={(e) => [setEmail(e.target.value), setError("")]}
+              />
+            </div>
+
+            <div className="flex flex-col pb-4">
+              <label className="label" htmlFor="senha">
+                Senha
+              </label>
+              <input
+                className="input"
+                type="password"
+                value={senha}
+                name="senha"
+                onChange={(e) => [setSenha(e.target.value), setError("")]}
+              />
+            </div>
+
+            <div className="flex flex-col pb-4">
+              <label className="label" htmlFor="telefone">
+                Telefone
+              </label>
+              <input
+                className="input"
+                type="text"
+                value={telefone}
+                name="telefone"
+                onChange={(e) => [setTelefone(e.target.value), setError("")]}
+              />
+            </div>
+
+            <div className="flex flex-col pb-4">
+              <label className="label" htmlFor="idade">
+                Idade
+              </label>
+              <input
+                className="input"
+                type="number"
+                value={idade}
+                name="idade"
+                onChange={(e) => [
+                  setIdade(parseInt(e.target.value)),
+                  setError(""),
+                ]}
+              />
+            </div>
+
+            <div className="flex flex-col pb-4">
+              <label className="label" htmlFor="sexo">
+                Sexo
+              </label>
+              <select
+                className="input"
+                value={sexo}
+                name="sexo"
+                id="sexo"
+                onChange={(e) => setSexo(e.target.value)}
+              >
+                <option value="Feminino">Feminino</option>
+                <option value="Masculino">Masculino</option>
+                <option value="Prefiro não responder">
+                  Prefiro não responder
+                </option>
+              </select>
+            </div>
+
+            <div className="md:grid md:grid-cols-2">
+              <div className="mb-4">
+                <span className="label">Pratica atividade física?</span>
+                <div className="">
+                  <label className="mr-2" htmlFor="sim">
+                    Sim
+                  </label>
+                  <input
+                    className="mr-2"
+                    type="radio"
+                    name="atividadeFisica"
+                    id="sim"
+                    onClick={(e) => {
+                      setPraticaAtividade(true);
+                    }}
+                  />
+                  <label className="mr-2" htmlFor="não">
+                    Não
+                  </label>
+                  <input
+                    type="radio"
+                    name="atividadeFisica"
+                    id="não"
+                    onClick={(e) => {
+                      setPraticaAtividade(false);
+                    }}
+                  />
+                </div>
+              </div>
+
+              <div className="mb-4">
+                <span className="label">Tem fraqueza muscular?</span>
+                <div>
+                  <label className="mr-2" htmlFor="sim">
+                    Sim
+                  </label>
+                  <input
+                    className="mr-2"
+                    type="radio"
+                    name="fraquezaMuscular"
+                    id="sim"
+                    onClick={(e) => {
+                      setFraquezaMuscular(true);
+                    }}
+                  />
+                  <label className="mr-2" htmlFor="não">
+                    Não
+                  </label>
+                  <input
+                    type="radio"
+                    name="fraquezaMuscular"
+                    id="não"
+                    onClick={(e) => {
+                      setFraquezaMuscular(false);
+                    }}
+                  />
+                </div>
+              </div>
+
+              <div className="mb-4">
+                <span className="label">Tem dificuldade respiratória?</span>
+                <div>
+                  <label className="mr-2" htmlFor="sim">
+                    Sim
+                  </label>
+                  <input
+                    className="mr-2"
+                    type="radio"
+                    name="dificuldadeRespiratoria"
+                    id="sim"
+                    onClick={(e) => {
+                      setDificuldadeRespiratoria(true);
+                    }}
+                  />
+                  <label className="mr-2" htmlFor="não">
+                    Não
+                  </label>
+                  <input
+                    type="radio"
+                    name="dificuldadeRespiratoria"
+                    id="não"
+                    onClick={(e) => {
+                      setDificuldadeRespiratoria(false);
+                    }}
+                  />
+                </div>
+              </div>
+
+              <div className="mb-4">
+                <span className="label">Tem perda de memória?</span>
+                <div className="">
+                  <label className="mr-2" htmlFor="sim">
+                    Sim
+                  </label>
+                  <input
+                    className="mr-2"
+                    type="radio"
+                    name="perdaDeMemoria"
+                    id="sim"
+                    onClick={(e) => {
+                      setPerdaDeMemoria(true);
+                    }}
+                  />
+                  <label className="mr-2" htmlFor="não">
+                    Não
+                  </label>
+                  <input
+                    type="radio"
+                    name="perdaDeMemoria"
+                    id="não"
+                    onClick={(e) => {
+                      setPerdaDeMemoria(false);
+                    }}
+                  />
+                </div>
+              </div>
+
+              <div className="mb-4">
+                <span className="label">
+                  Sofreu algum acidente nos últimos 12 meses?
+                </span>
+                <div>
+                  <label className="mr-2" htmlFor="sim">
+                    Sim
+                  </label>
+                  <input
+                    className="mr-2"
+                    type="radio"
+                    name="acidente12meses"
+                    id="sim"
+                    onClick={(e) => {
+                      setAcidenteEm12Meses(true);
+                    }}
+                  />
+                  <label className="mr-2" htmlFor="não">
+                    Não
+                  </label>
+                  <input
+                    type="radio"
+                    name="acidente12meses"
+                    id="não"
+                    onClick={(e) => {
+                      setAcidenteEm12Meses(false);
+                    }}
+                  />
+                </div>
+              </div>
+
+              <div className="mb-4">
+                <span className="label">Tem hipertensão?</span>
+                <div>
+                  <label className="mr-2" htmlFor="sim">
+                    Sim
+                  </label>
+                  <input
+                    className="mr-2"
+                    type="radio"
+                    name="hipertensao"
+                    id="sim"
+                    onClick={(e) => {
+                      setHipertensao(true);
+                    }}
+                  />
+                  <label className="mr-2" htmlFor="não">
+                    Não
+                  </label>
+                  <input
+                    type="radio"
+                    name="hipertensao"
+                    id="não"
+                    onClick={(e) => {
+                      setHipertensao(false);
+                    }}
+                  />
+                </div>
+              </div>
+
+              <div className="mb-4">
+                <span className="label">Tem diabetes?</span>
+                <div>
+                  <label className="mr-2" htmlFor="sim">
+                    Sim
+                  </label>
+                  <input
+                    className="mr-2"
+                    type="radio"
+                    name="diabetes"
+                    id="sim"
+                    onClick={(e) => {
+                      setDiabetes(true);
+                    }}
+                  />
+                  <label className="mr-2" htmlFor="não">
+                    Não
+                  </label>
+                  <input
+                    type="radio"
+                    name="diabetes"
+                    id="não"
+                    onClick={(e) => {
+                      setDiabetes(false);
+                    }}
+                  />
+                </div>
+              </div>
+
+              <div className="mb-4">
+                <span className="label">Tem doenças cardiovasculares?</span>
+                <div>
+                  <label className="mr-2" htmlFor="sim">
+                    Sim
+                  </label>
+                  <input
+                    className="mr-2"
+                    type="radio"
+                    name="doencasCardio"
+                    id="sim"
+                    onClick={(e) => {
+                      setDoencasCardio(true);
+                    }}
+                  />
+                  <label className="mr-2" htmlFor="não">
+                    Não
+                  </label>
+                  <input
+                    type="radio"
+                    name="doencasCardio"
+                    id="não"
+                    onClick={(e) => {
+                      setDoencasCardio(false);
+                    }}
+                  />
+                </div>
+              </div>
+
+              <div className="mb-4">
+                <span className="label">Tem doenças pulmonares?</span>
+                <div>
+                  <label className="mr-2" htmlFor="sim">
+                    Sim
+                  </label>
+                  <input
+                    className="mr-2"
+                    type="radio"
+                    name="doencasPulmonares"
+                    id="sim"
+                    onClick={(e) => {
+                      setDoencasPulmonares(true);
+                    }}
+                  />
+                  <label className="mr-2" htmlFor="não">
+                    Não
+                  </label>
+                  <input
+                    type="radio"
+                    name="doencasPulmonares"
+                    id="não"
+                    onClick={(e) => {
+                      setDoencasPulmonares(false);
+                    }}
+                  />
+                </div>
+              </div>
+            </div>
+
+            <Error Message={error} />
+
+            <Button Text="Registrar" onClick={handleRegister} Type="submit" />
+          </form>
+        </div>
+      </main>
+      <Footer />
+    </div>
+  );
 };
 
-export default signup;
+export default Signup;
