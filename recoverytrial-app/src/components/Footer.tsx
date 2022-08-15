329e14bf5cb22a99eba850dc6cc0b93426dd0bd8
ayanfaustt <ayanfaustt@gmail.com>
2022-08-15 18:44:10 -0300

    feat: adição dos icones ao footer e incrementos na página de info do usuário

--- recoverytrial-app/src/components/Footer.tsx
+++ recoverytrial-app/src/components/Footer.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 import LogoUFRPE from "./logos/LogoUFRPE";
+import { BiPhone, BiMap } from 'react-icons/bi'
+import { HiOutlineMail } from 'react-icons/hi'
+import { IconContext } from "react-icons";
 
 
 const Footer = () => {
@@ -12,9 +15,22 @@ const Footer = () => {
         <div className="pl-56">
           <p className="font-bold text-blue-200 text-lg">Contato</p>
           <div className="pl-9 flex flex-col gap-2 mt-2 mb-3">
-            <p>Fone: </p>
-            <p>Endereço: Rua Dom Manuel de Medeiros, s/n - Dois Irmãos, Recife - PE, 54171-900</p>
-            <p>E-mail: </p>
+            <IconContext.Provider value={{ size: '25px' }}>
+              <div className="flex flex-row gap-2">
+                <BiPhone color="#FF8484" />
+                <p></p>
+              </div>
+
+              <div className="flex flex-row gap-2">
+                <BiMap color="#FF8484" />
+                <p>Rua Dom Manuel de Medeiros, s/n - Dois Irmãos, Recife - PE, 52171-900 </p>
+              </div>
+
+              <div className="flex flex-row gap-2">
+                <HiOutlineMail color="#FF8484" />
+                <p>recoverytrialapp@gmail.com</p>
+              </div>
+            </IconContext.Provider>
           </div>
         </div>
         <div className="m-auto border-t-2 border-blue-200 w-4/5">
