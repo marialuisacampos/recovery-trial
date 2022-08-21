fd6b86e9ba19f9f5d8ed55fbd389773ec3cc7bca
marialuisacampos <luisa.campos@ufrpe.br>
2022-08-20 21:19:10 -0300

    Fix token error

--- backend/src/database/db.ts
+++ backend/src/database/db.ts
@@ -1,6 +1,6 @@
 import { Sequelize } from "sequelize";
 
-export const db = new Sequelize("recovery", "root", "root", {
+export const db = new Sequelize("recovery", "root", "123456", {
   dialect: "mysql",
   host: process.env.DATABASE_HOST,
   port: process.env.DATABASE_PORT,
--- backend/src/middlewares/auth.ts
+++ backend/src/middlewares/auth.ts
@@ -1,26 +1,25 @@
-import {Request, Response, NextFunction, response } from 'express';
-import jwt from 'jsonwebtoken';
+import { Request, Response, NextFunction, response } from "express";
+import jwt from "jsonwebtoken";
 
 async function userAuth(req: Request, res: Response, next: NextFunction) {
-    try { 
-        
-        const {authorization} = req.headers;
-        const secret = process.env.SECRET;
+  try {
+    const { authorization } = req.headers;
+    const secret = process.env.SECRET;
 
-        if(!authorization) return req.status(401).json({message:'Token is required'});
+    if (!authorization)
+      return req.status(401).json({ message: "Token is required" });
 
-        const [ ,token] = authorization.split(' ');
+    const [, token] = authorization.split(" ");
 
-        try {
-            await jwt.verify(token, '1234123');
-            next();
-        } catch (error) {
-            return res.status(401).json({message:'Token is required'});
-        }
-
+    try {
+      await jwt.verify(token, "1234123");
+      next();
     } catch (error) {
-        return error;
+      return res.status(401).json({ message: "Token is required" });
     }
+  } catch (error) {
+    return res.status(400).json({ message: "Token is required" });
+  }
 }
 
 export default userAuth;
