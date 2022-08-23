0be2b05f398a38509f123bfe79dc01aa39479c80
marialuisacampos <luisa.campos@ufrpe.br>
2022-08-23 17:24:37 -0300

    Add cors at backend

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,16 @@
 import express from "express";
 import { db } from "./database/db";
 import { router } from "./routes";
+import cors from "cors";
 
 const app = express();
 app.use(express.json());
+app.use(
+  cors({
+    origin: "*",
+    optionsSuccessStatus: 200,
+  })
+);
 app.use(router);
 
 app.listen(3001, async () => {
