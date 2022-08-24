410bc118b61fbbe12119269dd210134b1fae5520
marialuisacampos <luisa.campos@ufrpe.br>
2022-08-24 17:56:15 -0300

    add getserversideprops at main page

--- recoverytrial-app/src/pages/main/[user].tsx
+++ recoverytrial-app/src/pages/main/[user].tsx
@@ -1,5 +1,5 @@
 
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { GetServerSideProps } from "next";
 import Footer from "../../components/Footer";
 import Navbar from "../../components/Navbar";
@@ -9,67 +9,100 @@ import Memory from "../../components/cards/Memory";
 import { useRouter } from "next/router";
 import axios from "axios";
 import Link from "next/link";
+import { getUserLocalStorage } from "../../context/AuthProvider/util";
 
-type Props = {};
+interface Props {
+  links: any
+  treinos: string[]
+}
 
-export default function Main() {
-  const router = useRouter();
-  const { user } = router.query;
-
-  const [treinos, setTreinos] = useState<string[]>(["flexibilidade", "forca"])
-  const [links, setLinks] = useState<any>({})
-  const [nivel, setNivel] = useState<string>()
-
-  const fetchData = async () => {
-    const userLogged = localStorage.getItem("u");
-    const userLoggedObject = JSON.parse(userLogged!);
-    await axios.get(`http://localhost:3001/users/${user}`, {
-      headers: {
-        'authorization': `Bearer ${userLoggedObject.token}`,
-      },
-    }).then(userData => {
-      if (userData.data.perda_de_memoria) { treinos.push("dualtask") }
-      if (userData.data.dificuldade_respiratoria) { treinos.push("aerobio") }
-      if (userData.data.acidente_em_12_meses) { treinos.push("equilibrio") }
-      userData.data.p_atividade_fisica ? setNivel("avancado") : setNivel("iniciante")
-    }).then((data) => {
-      treinos.forEach(async (treino: any) => {
-        const fetchVideos = await axios.get(`http://localhost:3001/videos/${treino}`, {
-          headers: {
-            'authorization': `Bearer ${userLoggedObject.token}`,
-          },
-        });
-        links[treino] = fetchVideos.data[0].videoId
-      })
-    })
-  };
-
-  useEffect(() => {
-    fetchData();
-  }, [() => fetchData()]);
+export default function Main({ treinos, links }: Props) {
 
   return (
     <div>
       <Navbar />
-      {
-        links.length > 1 ? (
-          <div className="flex  h-screen bg-black justify-center items-center">
-            <div className="bg-gray-600 mt-24 w-2/5 h-2/4 md:w-4/5 md:h-3/4 lg:w-4/5 lg:h-3/4 rounded-md ">
-              {treinos.map((t) => (
-                <Link href={`/training/${t}/${links[t]}`}>{links[t]}</Link>
-              ))
-              }
-            </div>
-          </div>
-        ) : (
-          <div className="flex  h-screen bg-black justify-center items-center">
-            <div className="bg-gray-600 mt-24 w-2/5 h-2/4 md:w-4/5 md:h-3/4 lg:w-4/5 lg:h-3/4 rounded-md ">
-              <p>Carregando...</p>
-            </div>
-          </div>
-        )
-      }
+      <div className="flex  h-screen bg-black justify-center items-center">
+        <div className="bg-gray-600 mt-24 w-2/5 h-2/4 md:w-4/5 md:h-3/4 lg:w-4/5 lg:h-3/4 rounded-md ">
+          {treinos.map((t) => (
+            <Link href={`/training/${t}/${links[t]}`}>{t}</Link>
+          ))
+          }
+        </div>
+      </div>
       <Footer />
     </div>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const user = context.params.user;
+
+  let treinos = ["flexibilidade", "forca"]
+  let links = {
+    flexibilidade: "",
+    forca: "",
+    dualtask: "",
+    aerobio: "",
+    equilibrio: ""
+  }
+
+  const token = context.req.cookies["token"]
+
+  const userRequest = await axios.get(`http://localhost:3001/users/${user}`, {
+    headers: {
+      'authorization': `Bearer ${token}`,
+    },
+  })
+
+  if (userRequest.data.perda_de_memoria) {
+    treinos.push("dualtask")
+    const fetchVideos = await axios.get(`http://localhost:3001/videos/dualtask`, {
+      headers: {
+        'authorization': `Bearer ${token}`,
+      },
+    });
+    links["dualtask"] = fetchVideos.data[0].videoId
+  }
+
+  if (userRequest.data.dificuldade_respiratoria) {
+    treinos.push("aerobio")
+    const fetchVideos = await axios.get(`http://localhost:3001/videos/aerobio`, {
+      headers: {
+        'authorization': `Bearer ${token}`,
+      },
+    });
+    links["aerobio"] = fetchVideos.data[0].videoId
+  }
+
+  if (userRequest.data.acidente_em_12_meses) {
+    treinos.push("equilibrio")
+    const fetchVideos = await axios.get(`http://localhost:3001/videos/equilibrio`, {
+      headers: {
+        'authorization': `Bearer ${token}`,
+      },
+    });
+    links["equilibrio"] = fetchVideos.data[0].videoId
+  }
+
+  const fetchVideosFlexibilidade = await axios.get(`http://localhost:3001/videos/flexibilidade`, {
+    headers: {
+      'authorization': `Bearer ${token}`,
+    },
+  });
+  links["flexibilidade"] = fetchVideosFlexibilidade.data[0].videoId
+
+  const fetchVideosForca = await axios.get(`http://localhost:3001/videos/forca`, {
+    headers: {
+      'authorization': `Bearer ${token}`,
+    },
+  });
+  links["forca"] = fetchVideosForca.data[0].videoId
+
+  return {
+    props: {
+      treinos,
+      links
+    },
+  };
+
 }
