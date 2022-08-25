3d6c922e86593fac9d89f649955f0eb4dd201d2a
marialuisacampos <luisa.campos@ufrpe.br>
2022-08-25 19:46:04 -0300

    Fix Video Player

--- recoverytrial-app/src/components/videos/Player.tsx
+++ recoverytrial-app/src/components/videos/Player.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { DefaultUi, Player, Youtube } from "@vime/react";
 import "@vime/core/themes/default.css";
+import ReactPlayer from "react-player"
 
 interface Props {
   videoId?: any;
@@ -8,10 +9,9 @@ interface Props {
 
 const PlayerVideo = ({ videoId }: Props) => {
   return (
-    <Player autoplay={true}>
-      <Youtube videoId={videoId} cookies={false} />
-      <DefaultUi />
-    </Player>
+    <div className="relative pt-[56.25%]">
+      <ReactPlayer url={`https://www.youtube.com/watch?v=${videoId}`} className="absolute top-0 left-0" width="100%" height="100%" playing={true} />
+    </div>
   );
 };
 
