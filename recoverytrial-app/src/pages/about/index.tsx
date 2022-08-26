dcc55a3ffd2914b932cab74fc666604197aaa4c8
marialuisacampos <luisa.campos@ufrpe.br>
2022-08-26 13:56:34 -0300

    Add text at about page

--- recoverytrial-app/src/pages/about/index.tsx
+++ recoverytrial-app/src/pages/about/index.tsx
@@ -14,31 +14,7 @@ const index = (props: Props) => {
             Recovery Trial
           </h1>
           <p className="text-sm md:text-base">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus
-            sunt quam possimus? Consectetur hic, excepturi debitis nobis fugit,
-            distinctio modi omnis placeat vitae iste blanditiis atque
-            aspernatur. Sed, dicta nam. Lorem ipsum dolor sit amet consectetur
-            adipisicing elit. Temporibus sunt quam possimus? Consectetur hic,
-            excepturi debitis nobis fugit, distinctio modi omnis placeat vitae
-            iste blanditiis atque aspernatur. Sed, dicta nam. Lorem ipsum dolor
-            sit amet consectetur adipisicing elit. Temporibus sunt quam
-            possimus? Consectetur hic, excepturi debitis nobis fugit, distinctio
-            modi omnis placeat vitae iste blanditiis atque aspernatur. Sed,
-            dicta nam. Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Temporibus sunt quam possimus? Consectetur hic, excepturi debitis
-            nobis fugit, distinctio modi omnis placeat vitae iste blanditiis
-            atque aspernatur. Sed, dicta nam. Lorem ipsum dolor sit amet
-            consectetur adipisicing elit. Temporibus sunt quam possimus?
-            Consectetur hic, excepturi debitis nobis fugit, distinctio modi
-            omnis placeat vitae iste blanditiis atque aspernatur. Sed, dicta
-            nam. Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Temporibus sunt quam possimus? Consectetur hic, excepturi debitis
-            nobis fugit, distinctio modi omnis placeat vitae iste blanditiis
-            atque aspernatur. Sed, dicta nam. Lorem ipsum dolor sit amet
-            consectetur adipisicing elit. Temporibus sunt quam possimus?
-            Consectetur hic, excepturi debitis nobis fugit, distinctio modi
-            omnis placeat vitae iste blanditiis atque aspernatur. Sed, dicta
-            nam.{" "}
+            Prezados, bem vindo ao Recovery App. Esta é uma plataforma construída para lhe ajudar na realização de exercícios físicos sem a supervisão direta de um profissional de Educação Física. Sabemos que após o COVID é comum a existência de alguns problemas, como: fraqueza muscular, cansaço, aumento do estresse e ansiedade, como também perda de memória. Portanto, os exercícios poderão auxiliar nisso. Sigam as instruções e mantenha sua saúde em dia.
           </p>
         </div>
       </main>
